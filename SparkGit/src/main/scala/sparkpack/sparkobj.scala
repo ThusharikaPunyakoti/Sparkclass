21b3141eb1d8803bd66ced5616784fdf9040387c
Thusharika <Thusharika@LAPTOP-FD84CJCH>
2021-06-21 20:59:02 +0530

    Object added

--- SparkGit/src/main/scala/sparkpack/sparkobj.scala
+++ SparkGit/src/main/scala/sparkpack/sparkobj.scala
@@ -1,5 +1,23 @@
 package sparkpack
 
+import org.apache.spark._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+
+
 object sparkobj {
   
+  def main(args:Array[String]):Unit={ 
+    
+    val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+					val sc = new SparkContext(conf)
+					sc.setLogLevel("ERROR") 
+				
+		val spark=SparkSession.builder().getOrCreate()
+		import spark.implicits._
+		
+		
+		
+  
+}
 }
